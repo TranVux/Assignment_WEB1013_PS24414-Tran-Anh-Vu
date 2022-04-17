a15f866acee2f5f0f9a5c946758ccd68b9a4fea5
TranVux <vutran789jjjj@gmail.com>
2022-04-17 17:45:47 +0700

    Update BG-layer and Function mobile_menu

--- ASM/scripts/main.js
+++ ASM/scripts/main.js
@@ -1,6 +1,14 @@
-let nav_btn = document.querySelector('.header-nav-btn');
-let nav_menu = document.querySelector(".nav-menu_mobile");
+var nav_btn = document.querySelector('.header-nav-btn');
+var nav_menu = document.querySelector(".nav-menu_mobile");
+var bg_layer = document.querySelector(".gb-layer");
+function turnOnOffMenuMobile() {
+    nav_btn.classList.toggle("active");
+    nav_menu.classList.toggle("show");
+    bg_layer.classList.toggle("show")
+}
 nav_btn.onclick = function () {
-    nav_btn.classList.toggle('active');
-    nav_menu.classList.toggle('show');
+    turnOnOffMenuMobile();
+}
+bg_layer.onclick = function () {
+    turnOnOffMenuMobile();
 }
